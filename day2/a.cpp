e0687c4e679881327d6b8896618938ae1b31d25c
Ajay <ajay.patidar.4632@gmail.com>
2024-12-07 18:53:33 +0530

    day2 solved

---
+++ day2/a.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <fstream>
+#include<sstream>
+#include<vector>
+#include<algorithm>
+#include <cmath>
+
+using namespace std;
+bool check_difference(vector<int> a)
+{
+    
+    for(int i=0; i<a.size()-1; i++){
+        if(1>abs(a[i]-a[i+1]) || 3<abs(a[i]-a[i+1])){
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    // Create an ifstream object to read from a file
+    ifstream inputFile("data.txt");
+
+    // Check if the file was opened successfully
+    if (!inputFile) {
+        cerr << "Unable to open file for reading";
+        return 1;
+    }
+
+    // Read from the file
+    string line;
+    vector<vector<int>> list;
+    while (getline(inputFile, line)) {
+      // split the line
+         istringstream iss(line);
+         vector<int>temp;
+        int num;
+        while(iss>> num){
+            temp.push_back(num);
+        }
+        list.push_back(temp);
+        
+    }
+    int count=0;
+    for(const auto& row : list){
+         if ((is_sorted(row.begin(), row.end()) || is_sorted(row.begin(), row.end(), greater<int>())) && check_difference(row)) {
+            count++;
+        }
+       
+
+    }
+    cout<<count;
+    // Close the file
+    inputFile.close();
+
+    
+
+    return 0;
+}
